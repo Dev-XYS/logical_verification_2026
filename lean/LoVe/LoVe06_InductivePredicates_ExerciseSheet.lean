0da1c1196e0fa017358d0609384a10de160a0c6d
Yushuo Xiao <yushuo.xiao@inf.ethz.ch>
2026-05-11 11:39:28 +0200

    question 1 of exercise 6

--- lean/LoVe/LoVe06_InductivePredicates_ExerciseSheet.lean
+++ lean/LoVe/LoVe06_InductivePredicates_ExerciseSheet.lean
@@ -35,19 +35,35 @@ Hint: `cases` or `induction` is useful to reason about hypotheses of the form
 
 @[simp] theorem Odd_1 :
     Odd 1 :=
-  sorry
+  by
+    intro h
+    cases h
 
 /- 1.2. Prove that 3 and 5 are odd. -/
 
 -- enter your answer here
+theorem Odd_3 :
+    Odd 3 :=
+  by
+    intro h
+    cases h
+    apply Odd_1
+    trivial
+
+theorem Odd_5 :
+    Odd 5 :=
+  by
+    intro h
+    cases h
+    apply Odd_3
+    trivial
 
 /- 1.3. Complete the following proof by structural induction. -/
 
 theorem Even_two_times :
     ∀m : ℕ, Even (2 * m)
   | 0     => Even.zero
-  | m + 1 =>
-    sorry
+  | m + 1 => by simp [mul_add]; apply Even.add_two; apply Even_two_times
 
 
 /- ## Question 2: Tennis Games
